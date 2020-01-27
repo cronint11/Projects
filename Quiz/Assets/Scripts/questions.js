eba6fa200e9ee64a334ae0b40947726ca1bfa491
Six4 <tm.timcronin@gmail.com>
2020-01-27 16:20:53 -0500

    added bones to project

---
+++ Quiz/Assets/Scripts/questions.js
@@ -0,0 +1,22 @@
+//
+
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+        type: "radio"
+    },
+    {
+        question: "The condition of an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+        type: "radio"
+    },
+    {
+        question: "Timed quizzes like these are effective in deterministic evaluation:",
+        choices: ["true", "false", "they're an arbitrary solution to harvard law", "this class assignment validates a system with many bugs incorporated as features"],
+        answer: ["false", "they're an arbitrary solution to harvard law", "this class assignment validates a system with many bugs incorporated as features"],
+        type: "checkbox"
+    }
+  ];
---
+++ Quiz/inspiration.md
@@ -0,0 +1,12 @@
+the quiz will ask questions as the hw assn intends.
+winning the quiz will be escaping the matrix.
+answering questions will take you through different screens.
+the intent will be to show the user how ridiculous these types of quizzes are.
+
+checklist:
+-question queue
+-matrix animation
+-matrix background
+-timer
+
+<a href="https://www.vecteezy.com/free-vector/words-sticker">Words Sticker Vectors by Vecteezy</a>
