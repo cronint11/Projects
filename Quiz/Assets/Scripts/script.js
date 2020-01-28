3e3d903a6b738bbe3babeddeb91932514610fd59
Six4 <tm.timcronin@gmail.com>
2020-01-27 19:17:14 -0500

    Start button working - need to add timer, and finish questions

--- Quiz/Assets/Scripts/script.js
+++ Quiz/Assets/Scripts/script.js
@@ -1,4 +1,31 @@
+var questionCount = 0;
+var gradeCount = 0;
+
 $("#start").on("click", function(){
-    alert("boom");
+    InsertQuestion();
     $("#timer").text("####");
 });
+
+function InsertQuestion() {
+    var htmlToAdd = "<span>question</span><br><br><input type='radio' value='1'>1<br><input type='radio' value='2'>2<br><button id='answer'>Answer</answer>";
+    $("#myDiv").empty();
+    $("#myDiv").append(htmlToAdd);
+}
+
+$("#answer").on("click", function(){
+    alert("here");
+    questionCount++;
+    GradeAnswer();
+    if(questionCount<questions.length)
+    {
+        alert("Game Over");
+    } else
+    {
+        InsertQuestion();
+    }
+});
+
+function GradeAnswer() {
+   gradeCount++;
+   
+}
