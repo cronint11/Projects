0e9a55cdc10032d5a2c512bbaf6d2ac1896333a8
Six4 <tm.timcronin@gmail.com>
2020-02-03 02:23:38 -0500

    Calendar project

---
+++ Calendar/Develop/script.js
@@ -0,0 +1,112 @@
+$(".date").html(moment().format("dddd")+" - "+moment().format("MMM Do YYYY"));
+
+var currentHour=moment().format('h a').split(" ");
+var hourIDs=["#9am","#10am","#11am","#12pm","#1pm","#2pm","#3pm","#4pm","#5pm"];
+var placeHolder=0;
+
+console.log(currentHour);
+ColorTimeSlots();
+
+function ColorTimeSlots(){
+    hourIDs.forEach(element => {
+        $(element).removeClass("past");
+        $(element).removeClass("present");
+        $(element).removeClass("future");
+    })
+    if (placeHolder==0) {
+        if (currentHour[1]=="pm") {
+            for(let i=0;i<3;i++){
+                $(hourIDs[i]).addClass("past");
+            }
+            if(currentHour[0]=="12") {
+                // it's the noon hour
+                $("#12pm").addClass("present");
+                for(let i=4;i<hourIDs.length;i++) {
+                    $(hourIDs[i]).addClass("future");
+                }
+            } else {
+                // it's after noon
+                $("#12pm").addClass("past");
+                for(let i=4;i<hourIDs.length;i++) {
+                    if (parseInt(hourIDs[i].substring(1))<parseInt(currentHour[0])) {
+                        // hourIDs[i] has past
+                        $(hourIDs[i]).addClass("past");
+                    } else if (parseInt(hourIDs[i].substring(1))==parseInt(currentHour[0])){
+                        // hourIDs[i] is current
+                        $(hourIDs[i]).addClass("present");
+                    } else {
+                        // hourIDs[i] is in the future
+                        $(hourIDs[i]).addClass("future");
+                    }
+                    
+                }
+            }
+        } else /* it's am */ {
+            if(parseInt(currentHour[0])<9 || currentHour[0]=="12"){
+                hourIDs.forEach(element => {
+                    // start of the day!
+                    $(element).addClass("future");
+                });
+            } else {
+                hourIDs.forEach(element => {
+                    if(parseInt(element.substring(1))<parseInt(currentHour[0]) && element.includes("am")) {
+                        // element is prior to current time
+                        $(element).addClass("past");
+                    } else if (parseInt(element.substring(1))==parseInt(currentHour[0])) {
+                        // element is current time
+                        $(element).addClass("present");
+                    } else {
+                        // element is in the future
+                        $(element).addClass("future");
+                    }
+                });
+            }
+
+        }
+    } else if (placeHolder<0) {
+        hourIDs.forEach(element => {
+            $(element).addClass("past");
+        })
+    } else {
+        hourIDs.forEach(element => {
+            $(element).addClass("future");
+        })
+    }
+}
+
+function LoadSchedule(){
+
+}
+
+function LoadDate(){
+    if (placeHolder<0) {
+        $(".date").html(moment().subtract(Math.abs(placeHolder), "days").format("dddd")+" - "+moment().subtract(Math.abs(placeHolder), "days").format("MMM Do YYYY"));
+    } else if (placeHolder==0) {
+        $(".date").html(moment().format("dddd")+" - "+moment().format("MMM Do YYYY"));
+    } else {
+        $(".date").html(moment().add(Math.abs(placeHolder), "days").format("dddd")+" - "+moment().add(Math.abs(placeHolder), "days").format("MMM Do YYYY"));
+    }
+}
+
+function DateChanged(){
+    ColorTimeSlots();
+    LoadDate();
+    LoadSchedule();
+}
+
+$(".prev").on("click", function() {
+    placeHolder--;
+    
+    DateChanged();
+});
+
+$(".next").on("click", function() {
+    placeHolder++;
+
+    DateChanged();
+})
+
+$(".saveBtn").on("click", function() {
+
+});
+
