a72255ad91770fb3574b8baf20e98976302d5b9d
Six4 <tm.timcronin@gmail.com>
2020-03-04 23:34:34 -0500

    Readme generator initialization

---
+++ ReadGen/index.js
@@ -0,0 +1,32 @@
+const questions = [{
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username: '
+    }, {
+        type: 'input',
+        name: 'project',
+        message: 'Project title: ',
+    }, {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project: '
+    }, {
+        type: 'input',
+        name: 'ToC',
+        message: 'Enter a Table of Contents: '
+    }, {
+        type: 'input',
+        name: 'installation',
+        message: ''
+    }
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
