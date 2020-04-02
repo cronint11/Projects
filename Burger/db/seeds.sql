b60497580db353f02a6b854fb14efe4d52e75226
Six4 <tm.timcronin@gmail.com>
2020-04-01 22:49:34 -0400

    initialization

---
+++ Burger/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Classic');
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whiskey River Chicken Burger', true);
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
