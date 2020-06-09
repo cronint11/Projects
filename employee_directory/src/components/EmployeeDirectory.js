7f7918858a53ebbbab7fd2834ed0ca9a42ef56f6
Six4 <tm.timcronin@gmail.com>
2020-06-08 22:44:03 -0400

    final push hopefully

--- employee_directory/src/components/EmployeeDirectory.js
+++ employee_directory/src/components/EmployeeDirectory.js
@@ -1,26 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import employees from "../utils/employees.json";
 
-class EmployeeDirectory extends Component {
-  state = {
-    // stored in utils\employees.json file created with https://randomuser.me/api/
-    employees
-  };
+function EmployeeDirectory() {
+  const [data, setData] = useState(employees);
+  const [sortType, setSortType] = useState('first');
+  const [filterType, setFilterType] = useState('');
 
-  componentDidMount(){
-    console.log(this.state.employees);
-  };
-
-
-  render() {
-    return (
+  return (
+    <div>
+      <select onChange={(e) => setSortType(e.target.value)}>
+        <option value="first">First Name</option>
+        <option value="last">Last Name</option>
+      </select>
+      <select onChange={(e) => setFilterType(e.target.value)}>
+        <option value="">filter...</option>
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+      </select>
       <ul>
-        {this.state.employees.map((e,index)=> (
-          <li key={index}><img src={e.employee.picture.thumbnail}></img>{index} {e.employee.gender==="female"?"f":"m"} {e.employee.name.first} {e.employee.name.last}</li>
+        {data.sort((a, b) => b.employee[sortType] > a.employee[sortType] ? -1 : 1).filter(({employee}) => !filterType || employee.gender.toLowerCase()===filterType.toLowerCase()).map((e, index) => (
+          <li key={index}><img src={e.employee.picture.thumbnail} alt={`${e.employee.first} ${e.employee.last}`} />{index} {e.employee.gender === "female" ? "f" : "m"} {e.employee.first} {e.employee.last}</li>
         ))}
       </ul>
-    );
-  };
+    </div>
+  );
 };
 
 export default EmployeeDirectory;
