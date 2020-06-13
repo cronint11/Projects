7c35c74e0b89bf2aedba497685793c44971f27fb
Six4 <tm.timcronin@gmail.com>
2020-06-13 05:35:54 -0400

    working!

--- Notes/server.js
+++ Notes/server.js
@@ -1,53 +1,86 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const fs = require('fs');
 
+const app = express();
 const port = 8080;
 
 var notes = [];
 
-app.listen(port,function(){
-    console.log(`listening to port ${port}`);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', function (request, response) {
+    // fs.readFile('./public/notes.html','utf8',(err, data)=>{
+    //     if (err)
+    //         throw err;
+    //     response.writeHead(200, { "Content-Type": "text/html" });
+    //     data = loadNotes(data);
+    //     response.end(data);
+    // });
+
+    response.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('/notes', function(request,response){
-    fs.readFile('./public/notes.html','utf8',(err, data)=>{
-        if (err)
-            throw err;
-        response.writeHead(200, { "Content-Type": "text/html" });
-        data = loadNotes(data);
-        response.end(data);
-    });
-});
-
-app.get('*', function(request,response){
-    fs.readFile('./public/index.html','utf8',(err, data)=>{
-        if (err)
-            response.end(err);
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(data);
-    });
-});
-
-const loadNotes = function (html) {
-    
-    if(notes.length == 0)
-    {
-        try {
-            let data = fs.readFileSync(`./db/db.json`, 'utf8');
-               
-            notes=JSON.parse(data);
-               
-        } catch (err) {
+app.get('/api/notes', function (request, response) {
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) {
             console.log(err);
-            return html.replace(`plac3ho1d3R`,``);
+        } else {
+            notes = JSON.parse(data);
+            response.json(notes);
         }
+    });
+});
+
+app.post('/api/notes', function (request, response) {
+    if(!request.body.title) {
+        return response.status(400).json({ msg: 'Title required.' });
+    }
+    if(!request.body.text){
+        return response.status(400).json({ msg: 'Note required.' });
     }
 
-    let htmlToAdd='';
-    notes.forEach(note => {
-        htmlToAdd += `<b>${note.title}</b><li>${note.text}</li>`;
+    const newNote = {
+        id: Date.now(),
+        title: request.body.title,
+        text: request.body.text
+    };
+
+    notes.push(newNote);
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+        if (err) {
+            response.json(err);
+        } else {
+            response.json(newNote);
+        }
+
     });
-    
-    return html.replace(`plac3ho1d3R`,htmlToAdd);
-};
+});
+
+app.delete('/api/notes/:id', function (request, response) {
+    notes.splice(notes.findIndex(note => note.id === request.params.id), 1);
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+        if (err) {
+            response.json(err);
+        } else {
+            response.json(notes);
+        }
+    })
+});
+
+app.get('/*', function (request, response) {
+    // fs.readFile('./public/index.html','utf8',(err, data)=>{
+    //     if (err)
+    //         response.end(err);
+    //     response.writeHead(200, { "Content-Type": "text/html" });
+    //     response.end(data);
+    // });
+
+    response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, function () {
+    console.log(`listening to port ${port}`);
+});
